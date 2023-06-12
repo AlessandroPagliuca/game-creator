b277d99e8b3c36e4306a1c4d7a63f36177185bd8
anashamimi <anashamimi2@gmail.com>
2023-06-12 17:58:38 +0200

    fix migration

---
+++ database/migrations/2023_06_12_155430_add_type_to_characters.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('characters', function (Blueprint $table) {
+            $table->unsignedBigInteger('type')->nullable();
+            $table->foreign('type')->references('id')->on('types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('characters', function (Blueprint $table) {
+            $table->dropForeign(['type']);
+            $table->dropColumn('type');
+        });
+    }
+};
