d3bd60ec1e406af9cef567c52c98808ebd5ab138
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-06-13 01:08:00 +0200

    add background pattern

--- resources/views/admin/characters/index.blade.php
+++ resources/views/admin/characters/index.blade.php
@@ -9,43 +9,48 @@
             </div>
             <div class="row">
                 @foreach ($characters as $character)
-                <div class="col-4">
-                    <div class="card card-bg mb-3 shadow">
-                        <div class="row align-items-center">
-                            <div class="col-10">
-                                <h4 class="fs-4 card-title fw-bold text-center text-capitalize py-2">{{ $character->name }}</h4>
+                    <div class="col-4">
+                        <div class="card card-bg mb-3 shadow">
+                            <div class="row align-items-center">
+                                <div class="col-10">
+                                    <h4 class="fs-4 card-title fw-bold text-center text-capitalize py-2">
+                                        {{ $character->name }}</h4>
+                                </div>
+                                <div class="col-2">
+                                    <p class="card-text text-small fw-bold">{{ $character->level }} lv.</p>
+                                </div>
+
                             </div>
-                            <div class="col-2">
-                                <p class="card-text text-small  fw-bold">{{ $character->level }} lv.</p>
-                            </div>
+                            <div class="card-body">
 
-                        </div>
-                        <div class="card-body">
+                                <p class="card-text"><span class="fw-bold text-capitalize">race:</span>
+                                    {{ $character->race }}</p>
+                                <p class="card-text"><span class="fw-bold text-capitalize">class:</span>
+                                    {{ $character->class }}</p>
+                                <p class="card-text"><span class="fw-bold text-capitalize">lifepoint:</span>
+                                    {{ $character->lifepoint }}</p>
+                                <p class="card-text"><i class="fa-solid fa-dumbbell"></i>: {{ $character->strength }} </p>
+                                <p class="card-text"><i class="fa-solid fa-person-running"></i>: {{ $character->agility }}
+                                </p>
+                                <p class="card-text"><span class="fw-bold text-capitalize">weapon:</span>
+                                    {{ $character->main_weapon }}</p>
 
-                            <p class="card-text"><span class="fw-bold text-capitalize">race:</span> {{ $character->race }}</p>
-                            <p class="card-text"><span class="fw-bold text-capitalize">class:</span> {{ $character->class }}</p>
-                            <p class="card-text"><span class="fw-bold text-capitalize">lifepoint:</span> {{ $character->lifepoint }}</p>
-                            <p class="card-text"><i class="fa-solid fa-dumbbell"></i>: {{ $character->strength }} </p>
-                            <p class="card-text"><i class="fa-solid fa-person-running"></i>: {{ $character->agility }} </p>
-                            <p class="card-text"><span class="fw-bold text-capitalize">weapon:</span> {{ $character->main_weapon }}</p>
 
+                                <p>{{ $character->type ? $character->type->name : 'No type' }}</p>
 
-                            <p>{{ $character->type ? $character->type->name : 'No type' }}</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a class="m-1" href="{{ route('admin.characters.show', $character->id) }}"><button
+                                            class="btn btn-warning"> Show</button></a>
 
-                            <div class="d-flex justify-content-between align-items-center">
-                                <a class="m-1" href="{{ route('admin.characters.show', $character->id) }}"><button
-                                        class="btn btn-warning"> Show</button></a>
-
-                                <form action="{{ route('admin.characters.destroy', $character->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="btn btn-danger m-1" type="submit">Delete</button>
-                                </form>
+                                    <form action="{{ route('admin.characters.destroy', $character->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger m-1" type="submit">Delete</button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-
                 @endforeach
 
             </div>
